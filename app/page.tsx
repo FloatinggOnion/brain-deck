81626921028938d689164b93217477dfbcbd8765
Ijeoma Ukonu <ukonu.ijeoma@stu.cu.edu.ng>
2024-08-20 17:53:47 +0100

    my changes

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Purchase our product</h1>
+      <a href="https://buy.stripe.com/test_6oEbKm091aL19occMM" target="_blank" rel="noopener noreferrer">
+        Pay Now
+      </a>
+    </div>
+  );
+}
+
+export default Home;
